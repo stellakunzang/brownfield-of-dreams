f99493e4dffe4c733a4a95d6cb7a6a41b5024e9a
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-05 20:09:16 -0600

    update variable name in controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,11 @@
 class FriendshipsController < ApplicationController
   def create
-    @friend = User.find_by(handle: params[:follower])
+    @friend = User.find_by(handle: params[:future_friend])
     params[:friend_id] = @friend.id
     friendship = Friendship.create(friendship_params)
     if friendship.save
       redirect_to dashboard_path
-    end 
+    end
   end
 
   private
