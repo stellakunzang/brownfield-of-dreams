f9cfb0c7c80bf77ccf4c69413f240da1abe72858
Ross Perry <perryr16@gmail.com>
2020-07-07 15:58:34 -0600

    attempt 6

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -5,15 +5,19 @@ class GithubService
 
   def conn
     # Faraday.new("https://api.github.com?access_token=#{@token}")
+    Faraday.new("https://api.github.com")
+
     # Faraday.new(url: 'https://api.github.com?access_token=#{token}') do |faraday|
     #   faraday.headers['Authorization'] = ENV['GITHUB_API_KEY']
-    Faraday.new("https://api.github.com")
-      faraday.headers['Authorization'] = "token #{[@token]}"
-    end
+    # Faraday.new("https://api.github.com")
+    #   faraday.headers['Authorization'] = "token #{[@token]}"
+    # end
   end
 
   def get_url(url)
-    response = conn.get("/user/#{url}")
+    response = conn.get("/user/#{url}") do |res|
+      res.headers['Authorization'] = "token #{@token}"
+    end
     JSON.parse(response.body, symbolize_names: true)
   end
 
