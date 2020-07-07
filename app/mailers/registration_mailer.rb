6919fefd254c14778b29dcc8a3672e36d8cada1d
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-06 19:00:02 -0600

    change route in email link

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,8 +2,8 @@ class RegistrationMailer < ApplicationMailer
 
   def confirmation_email
     @user = params[:user]
-    @url = 'https://brownfield-of-dreams-sb-rp.herokuapp.com/dashboard'
-    mail(to: @user.email, subject: "Please activate your new account")
+    @url = "https://brownfield-of-dreams-sb-rp.herokuapp.com/users/#{@user.id}/activate"
+    mail(to: @user.email, subject: 'Please activate your new account')
   end
 
 end
