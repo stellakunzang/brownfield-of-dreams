2fac1f11a42373f1be784d1e1e3af2019db9cb30
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-07 20:42:17 -0600

    create inverse friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,10 @@ class FriendshipsController < ApplicationController
     @friend = User.find_by(handle: params[:future_friend])
     params[:friend_id] = @friend.id
     friendship = Friendship.create(friendship_params)
-    redirect_to dashboard_path if friendship.save
+    mutual_friendship = Friendship.create(mutual_friendship_params)
+    if friendship.save && mutual_friendship.save
+      redirect_to dashboard_path
+    end
   end
 
   private
@@ -11,4 +14,8 @@ class FriendshipsController < ApplicationController
   def friendship_params
     params.permit(:user_id, :friend_id)
   end
+
+  def mutual_friendship_params
+    {user_id: params[:friend_id], friend_id: params[:user_id]}
+  end 
 end
