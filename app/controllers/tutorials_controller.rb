e9ca561a8d31cf0ed3a331a66aa29ef378b1c101
Ross Perry <perryr16@gmail.com>
2020-07-08 17:10:32 -0600

    redefine 404 error

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,7 +2,7 @@ class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
     if tutorial.classroom == true && current_user.nil?
-      redirect_to four_oh_four
+      four_oh_four
     else
       @facade = TutorialFacade.new(tutorial, params[:video_id])
     end
