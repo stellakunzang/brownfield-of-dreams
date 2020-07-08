5df8aa9c20eaf5d307489398f02a59e60231b14d
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-07 20:57:42 -0600

    display 404 when visitor tries to view classroom content

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,10 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if tutorial.classroom == true && current_user.nil?
+      redirect_to four_oh_four
+    else
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    end
   end
 end
